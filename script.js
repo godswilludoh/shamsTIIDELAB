903d410d19d8bfb160b736839f0e430c3d213e11
Godswill Udoh <godswilludoh@gmail.com>
2022-07-28 17:29:21 +0100

    refactored code base

--- script.js
+++ script.js
@@ -2,17 +2,17 @@
 let maxNumber = 200;
 
 for (let i = 1; i <= maxNumber; i++) {
-	// checking if number is divisible by 6 and not 8
-	if (i % 6 === 0 && i % 8 !== 0) {
+	//checking if number is divisible by both 8 and 6
+	if (i % 8 === 0 && i % 6 === 0) {
+		console.log('shamsTIIDELab');
+
+		// checking if number is divisible by 6 and not 8
+	} else if (i % 6 === 0 && i % 8 !== 0) {
 		console.log('Shams');
 
 		// checking if number is divisible by 8 and not 6
 	} else if (i % 8 === 0 && i % 6 !== 0) {
 		console.log('TIIDELab');
-
-		//checking if number is divisible by both 8 and 6
-	} else if (i % 8 === 0 && i % 6 === 0) {
-		console.log('shamsTIIDELab');
 	} else {
 		console.log(i);
 	}
